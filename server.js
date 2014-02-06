adf198f571f5d33a8bf482f9da8e5e11f2cbcc47
Gerad Suyderhoud <gerads@gmail.com>
2014-02-06 06:24:35 -0800

    fix

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ http.createServer(app).listen(port, function(err) {
     require('fs').stat(__filename, function(err, stats) {
       if (err) { return console.error(err); }
       process.setuid(stats.uid);
-      process.setgid(stats.uid);
     });
   }
 
