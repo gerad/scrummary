bfa2adff0580532a135d4377d19cd34cb3dff69c
Gerad Suyderhoud <gerads@gmail.com>
2014-02-06 07:22:59 -0800

    save data (and allow urlencoded posts)

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
 var port = (process.env.NODE_ENV === 'production' ? 80 : 8000);
 
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.logger());
 
 app.get('/ok', function(req, res, next) {
   res.send(200);
@@ -12,7 +17,9 @@ app.get('/ok', function(req, res, next) {
 
 app.post('/hooks/github', function(req, res, next) {
   console.log(req.body);
-  res.send(202);
+  save(req.body, function() {
+    res.send(202);
+  });
 });
 
 http.createServer(app).listen(port, function(err) {
@@ -28,3 +35,13 @@ http.createServer(app).listen(port, function(err) {
 
   console.log('Server running at http://0.0.0.0:' + port + '/');
 });
+
+function save(data, next) {
+  var day = new Date().toJSON().slice(0, 10);
+  var dir = path.join(__dirname, 'data', day);
+  mkdirp(dir, function(error) {
+    if (error) { return next(error); }
+    var file = path.join(dir, +new Date() + '');
+    fs.writeFile(file, JSON.stringify(data), next);
+  });
+}
